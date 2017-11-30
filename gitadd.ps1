4a7ddb8b225b06562d1f93091fda173137e404a5
vgangadharan <vgangadharan@spscommerce.com>
2017-11-30 14:42:38 -0500

    new

--- gitadd.ps1
+++ gitadd.ps1
@@ -3,4 +3,4 @@ git config --global user.email "vgangadharan@spscommerce.com"
 cd D:/migrations
 git add 'Test_Undo*'
 git commit -m "undo added"
-git push origin master
+git push origin master --force
