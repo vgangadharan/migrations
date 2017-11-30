49bbbb04e152e5e563e3609d1da3740e0a138d79
vgangadharan <vgangadharan@spscommerce.com>
2017-11-30 13:32:37 -0500

    new

--- gitadd.ps1
+++ gitadd.ps1
@@ -1,6 +1,7 @@
 git config --global user.name "vgangadharan"
 git config --global user.email "vgangadharan@spscommerce.com"
+ git remote add origin https://github.com/vgangadharan/qc-automation.git
 cd D:/migrations
 git add 'Test_Undo*.mmp'
 git commit -m "undo added"
-git push  https://github.com/vgangadharan/migrations.git master
+git push  origin master
