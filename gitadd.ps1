7c0fcaf761a1a21754ec76a38667262161df1b6b
vgangadharan <vgangadharan@spscommerce.com>
2018-03-30 11:07:27 -0400

    new

--- gitadd.ps1
+++ gitadd.ps1
@@ -1,4 +1,6 @@
 cd D:/migrations
 Copy-Item -Path D:\migrations\Colehaan_Undo*.mmp -Destination D:\migrations\UndoPackage
+cd D:/migrations/UndoPackage
+git add 'Colehaan_Undo*'
 git commit -m "undo added"
 git push origin Colehaan
